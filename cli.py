86372fdad610fef5f23ec8298ffda03bcb51aef7
Wissam Mantash <42943707+wissam2000@users.noreply.github.com>
2021-03-25 00:44:12 -0400

    testing json Request

--- cli.py
+++ cli.py
@@ -1,4 +1,6 @@
 #requests is a requirment that needs to me installed (module requests)
 import requests
-r = requests.get('https://github.com/kennethreitz/requests/blob/master/README.rst')
-print(r.text)
+url = 'https://github.com/peaudecastor/data-converter-take-home-test/blob/main/sample.json'
+r = requests.get(url)#, headers={'accept': 'application/json'})
+
+print(r.json())
